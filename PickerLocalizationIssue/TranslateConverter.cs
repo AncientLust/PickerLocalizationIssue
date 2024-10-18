03b24c990c38c4b5cc3bf8eb0c6abe0f09f7d91a
Stephen Quan <squan@esri.com>
2024-10-18 19:00:40 +1100

    Refactor shorten TranslateConverter

--- PickerLocalizationIssue/TranslateConverter.cs
+++ PickerLocalizationIssue/TranslateConverter.cs
@@ -1,10 +1,9 @@
-﻿using PickerLocalizationIssue.Resources.Localization;
-using System.Globalization;
+﻿using System.Globalization;
 namespace PickerLocalizationIssue;
 public class TranslateConverter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-        => (value is string s) ? AppResources.ResourceManager.GetString(s) : null;
+        => (value is string s) ? PickerLocalizationIssue.Resources.Localization.AppResources.ResourceManager.GetString(s) : null;
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         => throw new NotImplementedException();
 }
